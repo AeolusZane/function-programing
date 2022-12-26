0db44ae4221790bc71f28a431ec75e9a15d5c769
AeolusZane <1393649219@qq.com>
2022-12-27 02:52:26 +0800

    new types

---
+++ 5.match_fold/index.ts
@@ -0,0 +1,16 @@
+import { fromPredicate, match } from "fp-ts/lib/Option";
+import { pipe } from 'fp-ts/lib/function';
+
+pipe(
+    2,
+    fromPredicate(value => value != 0),
+    match(() => 0, value => 10 / value), // 10/value 是非0返回的默认值
+    console.log
+)
+
+pipe(
+    0,
+    fromPredicate(value => value != 0),
+    match(() => 0, value => 10 / value), // 10/value 是非0返回的默认值
+    console.log
+)
