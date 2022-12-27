a5ef6067d2e7a7cc7fbbb596e33e58fb5b5b9e22
Aeolus.Zhang-张敬峥 <Aeolus.Zhang@fanruan.com>
2022-12-27 20:03:12 +0800

    alter

--- 6.form/valid.ts
+++ 6.form/valid.ts
@@ -16,23 +16,19 @@ const v1 = makeValidator([minLength(1)], 'phone number must not be empty');
 const v2 = makeValidator([startWith('+')], '前面请带加号');
 const v3 = makeValidator([minLength(8), maxLength(15)], '长度在8-15之间');
 const v4 = makeValidator([testPhoneNumberPattern], '格式不正确，请输入正确的数字');
+const resolve = (val: string) => {
+    console.log(`${minStringlen(val, 20)}✅:`, val);
+};
+const reject = (val: string, msg: string) => {
+    console.log(`${minStringlen(val, 20)}❌:`, msg);
+};
 
 // 最小字符串长度为15,不足补齐
 function minStringlen(str: string, len: number) {
     return str.padEnd(len, ' ');
 }
 
-const phoneNumberCheck = validationCheck(v1, v2, v3, v4)(
-    (val: string) => {
-        console.log(`${minStringlen(val, 20)}✅:`, val);
-    },
-    (val: string, msg: string) => {
-        console.log(`${minStringlen(val, 20)}❌:`, msg);
-    }
-);
-
-
-
+const phoneNumberCheck = validationCheck(v1, v2, v3, v4)(resolve, reject);
 
 phoneNumberCheck('+123456789'); // right
 phoneNumberCheck('123456789');  // left 前面没有加号
